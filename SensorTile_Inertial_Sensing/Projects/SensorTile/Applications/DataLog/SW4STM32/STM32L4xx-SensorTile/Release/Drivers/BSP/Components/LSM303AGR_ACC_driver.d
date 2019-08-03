de369e16ebe1a6e20e9c301e7af6ecc4396028d5
Kathleen Bryan <kathleenbryan24@gmail.com>
2019-08-03 13:26:56 -0400

    clean workspace

--- SensorTile_Inertial_Sensing/Projects/SensorTile/Applications/DataLog/SW4STM32/STM32L4xx-SensorTile/Release/Drivers/BSP/Components/LSM303AGR_ACC_driver.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/LSM303AGR_ACC_driver.o: \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_ACC_driver.c \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_ACC_driver.h
-
-/Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_ACC_driver.h:

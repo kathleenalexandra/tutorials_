f60ebcdb9f4a8f8cca129c21954a07f6c0ca98ef
Kathleen Bryan <kathleenbryan24@gmail.com>
2019-08-04 19:24:24 -0400

    default files

--- SensorTile_Inertial_Sensing/Projects/SensorTile/Applications/DataLog/SW4STM32/STM32L4xx-SensorTile/Release/Drivers/BSP/Components/LSM303AGR_MAG_driver.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/LSM303AGR_MAG_driver.o: \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_MAG_driver.c \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_MAG_driver.h
-
-/Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm303agr/LSM303AGR_MAG_driver.h:
--- SensorTile_Inertial_Sensing/Projects/SensorTile/Applications/DataLog/SW4STM32/STM32L4xx-SensorTile/Release/Drivers/BSP/Components/LSM6DSM_ACC_GYRO_driver.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/LSM6DSM_ACC_GYRO_driver.o: \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm6dsm/LSM6DSM_ACC_GYRO_driver.c \
- /Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm6dsm/LSM6DSM_ACC_GYRO_driver.h
-
-/Users/alexandra/Desktop/myworkspace/SensorTile_Inertial_Sensing/Drivers/BSP/Components/lsm6dsm/LSM6DSM_ACC_GYRO_driver.h:
